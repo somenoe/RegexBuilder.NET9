ddea305003c1dff317af9ce74a116fdfb9672ddd
somenoe <otashi24@gmail.com>
2025-10-21 06:24:06 +0700

    chore(cleanup): remove legacy VS 2010 test configuration files
    - deleted YuriyGuts.RegexBuilder.vsmdi (obsolete test metadata)
    - deleted Local.testsettings (legacy VS 2010 test settings)
    - deleted TraceAndTestImpact.testsettings (IntelliTrace settings)
    - updated RegexBuilder.slnx to remove references to deleted files
    - updated AssemblyInfo.cs InternalsVisibleTo from YuriyGuts.RegexBuilder.Tests to RegexBuilder.Tests

--- src/RegexBuilder/Properties/AssemblyInfo.cs
+++ src/RegexBuilder/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("YuriyGuts.RegexBuilder.Tests")]
+[assembly: InternalsVisibleTo("RegexBuilder.Tests")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
